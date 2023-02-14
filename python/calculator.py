f6f067197c94d14751e5f1470bea046ca2de2b7b
trace <ahmutysj3@gmail.com>
2023-02-13 20:01:47 -0500

    added weight converter python file

--- python/calculator.py
+++ python/calculator.py
@@ -6,8 +6,4 @@ print("Give me two numbers and I will add them up for you")
 num1 = float(input("NUMBER 1: "))
 num2 = float(input("NUMBER 2: "))
 total = num1 + num2
-print("The answer is " + str(total))
-animal = "lion"
-print("Name an animal and if it is the one I am thinking then I will tell you")
-animal2 = input("ANIMAL: ")
-print(animal2.lower() in animal)
+
